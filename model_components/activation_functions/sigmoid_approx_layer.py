('Atkins') 1. Finished implementation of approximate sigmoid layer by augmenting the layer with the gradients of both approximations 2. Created a rescaling layer to fix the unbounding occurring as a result of the approximation

model_components/activation_functions/sigmoid_approx_layer.py
@@ -4,19 +4,42 @@ from model_components.layers.layer_prototype import Layer
 
 
 class SigmoidApprox(Layer):
-    def __init__(self, deg=3):
+    def __init__(self, deg=7):
         self.X = None
         self.deg = deg
+        self.a_0, self.a_1, self.a_3 = 0.5, 1.20096, -0.81562
+        self.b_0, self.b_1, self.b_3, self.b_5, self.b_7 = (
+            0.5,
+            1.73496,
+            -4.19407,
+            5.4302,
+            -2.50739,
+        )
+        self.unsupported_exception_str = "The Sigmoid approximation layer was instantiated with an unsupported " \
+                                         "degree. Specify either degree 3 or degree 7 for this layer."
 
-    @staticmethod
-    def third_deg_approx(x):
-        a_0, a_1, a_3 = 0.5, 1.20096, -0.81562
-        return a_0 + a_1 * x / 8 + a_3 * np.power((x / 8), 3)
+    def third_deg_approx(self, x):
+        return self.a_0 + self.a_1 * x / 8 + self.a_3 * np.power((x / 8), 3)
 
-    @staticmethod
-    def seven_deg_approx(x):
-        b_0, b_1, b_3, b_5, b_7 = 0.5, 1.73496, -4.19407, 5.4302, -2.50739
-        return b_0 + b_1 * x / 8 + b_3 * np.power((x / 8), 3) + b_5 * np.power((x / 8), 5) + b_7 * np.power((x / 8), 7)
+    def third_deg_grad(self, x):
+        return self.a_1 / 8 + 3 * self.a_3 * np.power((x / 8), 2)
+
+    def seven_deg_grad(self, x):
+        return (
+            self.b_1 / 8
+            + 3 * self.b_3 * np.power((x / 8), 2)
+            + 5 * self.b_5 * np.power((x / 8), 4)
+            + 7 * self.b_7 * np.power((x / 8), 6)
+        )
+
+    def seven_deg_approx(self, x):
+        return (
+            self.b_0
+            + self.b_1 * x / 8
+            + self.b_3 * np.power((x / 8), 3)
+            + self.b_5 * np.power((x / 8), 5)
+            + self.b_7 * np.power((x / 8), 7)
+        )
 
     def forward(self, X):
         """
@@ -30,16 +53,13 @@ class SigmoidApprox(Layer):
         elif self.deg == 7:
             return self.seven_deg_approx(X)
         else:
-            raise Exception("The Sigmoid approximation layer was instantiated with an unsupported degree. Specify "
-                            "either degree 3 or degree 7 for this layer.")
+            raise Exception(self.unsupported_exception_str)
 
-    def backward(self, E, lr=0.001):
-        """
-        E_hat = sigmoid(X) * (1 - sigmoid(X))
-        """
-        t1 = np.multiply(
-            self.sigmoid(self.X), np.ones(shape=self.X.shape) - self.sigmoid(self.X)
-        )
-        return np.multiply(E, t1)
-
-
+    def backward(self, E):
+        if self.deg == 3:
+            grad = self.third_deg_grad(self.X)
+        elif self.deg == 7:
+            grad = self.seven_deg_grad(self.X)
+        else:
+            raise Exception(self.unsupported_exception_str)
+        return np.multiply(E, grad)

scripts/00-01-sigmoid-approx-comparison.py
@@ -3,25 +3,44 @@ import matplotlib.pyplot as plt
 
 
 def sigmoid(x):
-    return 1/(1 + np.exp(-x))
+    return 1 / (1 + np.exp(-x))
 
 
 def third_deg_approx(x):
     a_0, a_1, a_3 = 0.5, 1.20096, -0.81562
-    return a_0 + a_1 * x/8 + a_3 * np.power((x/8), 3)
+    return a_0 + a_1 * x / 8 + a_3 * np.power((x / 8), 3)
 
 
 def seven_deg_approx(x):
     b_0, b_1, b_3, b_5, b_7 = 0.5, 1.73496, -4.19407, 5.4302, -2.50739
-    return b_0 + b_1 * x/8 + b_3 * np.power((x/8), 3) + b_5 * np.power((x/8), 5) + b_7 * np.power((x/8), 7)
+    return (
+        b_0
+        + b_1 * x / 8
+        + b_3 * np.power((x / 8), 3)
+        + b_5 * np.power((x / 8), 5)
+        + b_7 * np.power((x / 8), 7)
+    )
 
 
 def taylor_approx(x):
-    c_0, c_1, c_3, c_5, c_7, c_9 = 1/2, 1/4, -1/48, 1/480, -17/80640, 31/1451520
-    return c_0 + c_1 * x + c_3 * np.power(x, 3) + c_5 * np.power(x, 5) + c_7 * np.power(x, 7) * c_9 * np.power(x, 9)
+    c_0, c_1, c_3, c_5, c_7, c_9 = (
+        1 / 2,
+        1 / 4,
+        -1 / 48,
+        1 / 480,
+        -17 / 80640,
+        31 / 1451520,
+    )
+    return (
+        c_0
+        + c_1 * x
+        + c_3 * np.power(x, 3)
+        + c_5 * np.power(x, 5)
+        + c_7 * np.power(x, 7) * c_9 * np.power(x, 9)
+    )
 
 
-x = np.arange(-8.1, 8.1, 0.1)
+x = np.arange(-10.1, 10.1, 0.1)
 sig_y = sigmoid(x)
 third_y = third_deg_approx(x)
 seven_y = seven_deg_approx(x)
@@ -38,4 +57,3 @@ plt.show()
 
 
 print("Hello World")
-